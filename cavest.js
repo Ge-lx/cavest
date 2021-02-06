80ab454a6edd7d182ea1e4db785e5918769f9d8c
Falk Ebert <falk.ebert@kip.uni-heidelberg.de>
2021-02-06 18:57:33 +0100

    Rework
    Now relies on github.com/ge-lx/sloth for visualization processing.
    
    * Reintroduce visualization control
    * Remove linux service control (might come back)
    * Remove config handling -> replaced with predefined configs
    
    Raspberyy Pi 4B installation lockfile

--- cavest.js
+++ cavest.js
@@ -5,104 +5,22 @@ const udp = require('dgram');
 const merge = require('deepmerge');
 const express = require('express');
 
-const config_path = '/home/pi/.config/cava/config';
-const ps_host = '192.168.178.175';
-
-const default_config = {
-	general: {
-		framerate: 50,
-		autosens: 0,
-		sensitivity: 12,
-		bars: 60,
-		lower_cutoff_freq: 40,
-		higher_cutoff_freq: 20000
-	},
-	input: {
-		method: 'alsa',
-		source: 'hw:1,1'
-	},
-	output: {
-		method: 'raw',
-		channels: 'mono',
-		raw_target: '/dev/stdout',
-		data_format: 'binary',
-		bit_format: '8bit'
-	},
-	smoothing: {
-		integral: 50,
-		gravity: 500
-	}
+const pixelhost = {
+	hostname: '192.168.178.175',
+	port: 8080
 };
 
-let current_config = default_config;
-
-const configs = {
-	default: default_config,
-	bright: merge(default_config, {
-		general: {
-			sensitivity: 60
-		}
-	}),
-	dim: merge(default_config, {
-		general: {
-			sensitivity: 5
-		}
-	}),
-	smooth: merge(default_config, {
-		smoothing: {
-			integral: 80,
-			gravity: 500
-		}
-	}),
-	hectic: merge(default_config, {
-		smoothing: {
-			integral: 10,
-			gravity: 1000
-		}
-	})
-};
-
-const write_config = (config, cb) => {
-	let str = '';
-	for (let name in config) {
-		str += `[${name}]\n`;
-		const section = config[name];
-		str += Object
-			.keys(section)
-			.map(key => `${key} = ${section[key]}`)
-			.join('\n') + '\n\n';
-	}
-
-	writeFile(config_path, str, (err) => {
-		current_config = config;
-	    if (err) {
-	    	// console.error(`Error writing configuration file: `, err)
-	    }
-    	console.log('Configuration updated.');
-    	cb();
-	});
-};
-
-const start_cava_service = () => {
-	exec('systemctl --user start cava.service');
-};
-
-const stop_cava_service = () => {
-	exec('systemctl --user stop cava.service');
-};
-
-const update_config = (config) => {
-	write_config(config, () => {
-		exec('bash -c "kill -SIGUSR1 $(pgrep cava)"');
-	});
+const slothhost = {
+	hostname: 'localhost',
+	port: 1236
 };
 
 const port = 1235;
 const app = express();
 
 let upd_socket = udp.createSocket('udp4');
-const send_socket_message = data => {
-	upd_socket.send(Buffer.from(data), 8080, ps_host, err => {
+const send_socket_message = (data, host = pixelhost) => {
+	upd_socket.send(Buffer.from(data), host.port, host.hostname, err => {
 		if (err) {
 			console.error('Error sending socket message: ', err);
 			try {
@@ -115,7 +33,13 @@ const send_socket_message = data => {
 	});
 };
 
-const SOCKET_MSG = {
+const SOCKET_MSG_SLOTH = {
+	start: 0,
+	stop: 1,
+	select: 2
+};
+
+const SOCKET_MSG_PIXEL = {
 	fill: 0,
 	clear: 1,
 	brightness: 2,
@@ -123,16 +47,34 @@ const SOCKET_MSG = {
 };
 
 const current_state = {
-	enabled: false,
+	visualization_enabled: false,
+	pixels_enabled: false,
 	brightness: 120,
 	color: { r: 0, g: 0, b: 0, w: 255 }
 };
+
+const start_sloth_service = () => {
+	current_state.pixels_enabled = false;
+	send_socket_message([SOCKET_MSG_SLOTH.start], slothhost);
+	current_state.visualization_enabled = true;
+};
+
+const stop_sloth_service = () => {
+	current_state.pixels_enabled = false;
+	send_socket_message([SOCKET_MSG_SLOTH.stop], slothhost);
+	current_state.visualization_enabled = false;
+};
+
+const select_config = (config) => {
+	send_socket_message([SOCKET_MSG_SLOTH.select, parseInt(config)], slothhost);
+};
+
 
 let interval_timer;
 const timed_shutoff = () => {
 	// console.log('Starting timer');
 	clearInterval(interval_timer);
-	if (current_state.enabled === false) {
+	if (current_state.pixels_enabled === false) {
 		return;
 	} else {
 		let brightness = current_state.brightness;
@@ -142,14 +84,14 @@ const timed_shutoff = () => {
 			brightness -= step;
 			// console.log('now: ', brightness);
 			if (brightness <= 1) {
-				current_state.enabled = false;
-				send_socket_message([SOCKET_MSG.clear]);
+				current_state.pixels_enabled = false;
+				send_socket_message([SOCKET_MSG_PIXEL.clear]);
 				clearInterval(interval_timer);
 				setTimeout(() => {
 					binary_clock_enabled = true;
 				}, 1000);
 			} else {
-				send_socket_message([SOCKET_MSG.brightness, Math.floor(brightness)]);
+				send_socket_message([SOCKET_MSG_PIXEL.brightness, Math.floor(brightness)]);
 			}
 		}, 100);
 	}
@@ -208,101 +150,103 @@ const binary_clock = (function () {
 	setInterval(loop, 1000);
 }());
 
+const ensure_visualization_disabled = () => {
+	if (current_state.visualization_enabled) {
+		stop_sloth_service();
+		current_state.visualization_enabled = false;
+	}
+};
+
 const disable_special_functions = () => {
 	clearInterval(interval_timer);
 	binary_clock_enabled = false;
 };
 
 app.get('/pixels/fill', (req, res, next) => {
-	// console.log('fill');
+	ensure_visualization_disabled();
 	disable_special_functions();
-	current_state.enabled = true;
-	send_socket_message([SOCKET_MSG.fill]);
+	send_socket_message([SOCKET_MSG_PIXEL.fill]);
+	current_state.pixels_enabled = true;
 	res.sendStatus(200);
 });
 
 app.get('/pixels/clear', (req, res, next) => {
-	// console.log('clear');
+	ensure_visualization_disabled();
 	disable_special_functions();
-	current_state.enabled = false;
-	send_socket_message([SOCKET_MSG.clear]);
+	setTimeout(() => {
+		send_socket_message([SOCKET_MSG_PIXEL.clear]);
+	}, 100);
+	current_state.pixels_enabled = false;
 	res.sendStatus(200);
 });
 
 app.get('/pixels/timer', (req, res, next) => {
-	// console.log('timer');
+	ensure_visualization_disabled();
 	disable_special_functions();
 	timed_shutoff();
 	res.sendStatus(200);
 });
 
 app.get('/pixels/clock', (req, res, next) => {
+	ensure_visualization_disabled();
 	disable_special_functions();
-	send_socket_message([SOCKET_MSG.clear]);
+	send_socket_message([SOCKET_MSG_PIXEL.clear]);
 	binary_clock_enabled = true;
 	res.sendStatus(200);
 });
 
 app.get('/pixels/brightness/:v', (req, res, next) => {
-	// console.log('brightness');
-	disable_special_functions();
 	current_state.brightness = req.params.v;
-	send_socket_message([SOCKET_MSG.brightness, req.params.v]);
+
+	if (current_state.pixels_enabled) {
+		disable_special_functions();
+		send_socket_message([SOCKET_MSG_PIXEL.brightness, req.params.v]);
+	}
 	res.sendStatus(200);
 });
 
 app.get('/pixels/color/:r/:g/:b/:w', (req, res, next) => {
-	// console.log('color');
-	disable_special_functions();
 	let c = req.params;
 	current_state.color = c;
-	send_socket_message([SOCKET_MSG.color, c.r, c.g, c.b, c.w]);
+
+	if (current_state.pixels_enabled) {
+		disable_special_functions();
+		send_socket_message([SOCKET_MSG_PIXEL.color, c.r, c.g, c.b, c.w]);
+	}
 	res.sendStatus(200);
 });
 
-app.get('/cava/start', (req, res, next) => {
-	start_cava_service();
+app.get('/visualization/start', (req, res, next) => {
+	start_sloth_service();
 	res.sendStatus(200)
 });
 
-app.get('/cava/stop', (req, res, next) => {
-	stop_cava_service();
+app.get('/visualization/stop', (req, res, next) => {
+	stop_sloth_service();
 	res.sendStatus(200);
 });
 
-app.get('/cava/config/:config', (req, res, next) => {
-	const conf_key = req.params.config;
-
-	if (configs.hasOwnProperty(conf_key)) {
-		update_config(configs[conf_key]);
-		res.sendStatus(200);
-	} else {
-		res.sendStatus(404);
-	}
+app.get('/visualization/config/:config', (req, res, next) => {
+	select_config(req.params.config);
+	res.sendStatus(200)
 });
 
-app.get('/cava/set/:section/:key/:value', (req, res, next) => {
-	const changed_config = {};
-	const changed_section = {};
-	const int_value = parseInt(req.params.value);
-	changed_section[req.params.key] = Number.isNaN(int_value) ? req.params.value : int_value;
-	changed_config[req.params.section] = changed_section;
+// app.get('/cava/set/:section/:key/:value', (req, res, next) => {
+// 	const changed_config = {};
+// 	const changed_section = {};
+// 	const int_value = parseInt(req.params.value);
+// 	changed_section[req.params.key] = Number.isNaN(int_value) ? req.params.value : int_value;
+// 	changed_config[req.params.section] = changed_section;
 
-	update_config(merge(current_config, changed_config));
-	res.sendStatus(200);
-});
+// 	select_config(merge(current_config, changed_config));
+// 	res.sendStatus(200);
+// });
 
-app.get('/status/pixels', (req, res, next) => {
+app.get('/status', (req, res, next) => {
 	res.json(current_state);
-});
-
-app.get('/status/config', (req, res, next) => {
-	res.json(current_config);
 });
 
 app.use(express.static('http_root'));
 app.listen(port, () => {
-	update_config(current_config);
-	start_cava_service();
 	console.log(`Cavest listening on port ${port}.`);
 });
